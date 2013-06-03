d95946ad5618edf09d60a5def75128f05e9f99c7
Sonia K. Gonzalez <songonza@gmail.com>
2013-06-03 16:55:18 -0400

    Initial App Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trustthis::Application.config.secret_token = 'acd46731cb5dbede4118037910e6a7fe7bb72a304888cd02cb35e1e31963fc129628b2351500946515b1d89d63fb85d39bfdd87595d1292e4d72ad9347498c04'
