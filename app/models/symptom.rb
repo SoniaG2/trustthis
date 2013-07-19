a9d23bea1e97fc9e2fd9ad2c2271ac99c089b4b6
Yunong Shao <wanderlust.by.night@gmail.com>
2013-07-19 21:12:06 +0800

    set default order to symptoms model

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,3 +1,9 @@
 class Symptom < ActiveRecord::Base
-  attr_accessible :content, :title
+  attr_accessible :content, :title, :order
+  validates :title, :order, uniqueness: true
+
+  before_create :set_order
+  def set_order
+    self.order = (Symptom.maximum(:order) || 0) + 1 unless self.order.present?
+  end
 end
