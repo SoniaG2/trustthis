4822fca7e4187875322df7e2a53a2bf41978b423
Yunong Shao <wanderlust.by.night@gmail.com>
2013-07-19 21:11:44 +0800

    edit symptoms controller and add http auth

--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,83 +1,56 @@
 class SymptomsController < ApplicationController
-  # GET /symptoms
-  # GET /symptoms.json
-  def index
-    @symptoms = Symptom.all
+  before_filter :authenticate, except: [:index, :show]
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @symptoms }
-    end
+  def index
+    @symptoms = Symptom.order("symptoms.order").all
   end
 
-  # GET /symptoms/1
-  # GET /symptoms/1.json
   def show
     @symptom = Symptom.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @symptom }
-    end
+    @next_symptom = Symptom.order("symptoms.order").where("symptoms.order > ?", @symptom.order).first
+    @previous_symptom = Symptom.order("symptoms.order").where("symptoms.order < ?", @symptom.order).first
   end
 
-  # GET /symptoms/new
-  # GET /symptoms/new.json
   def new
     @symptom = Symptom.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @symptom }
-    end
   end
 
-  # GET /symptoms/1/edit
   def edit
     @symptom = Symptom.find(params[:id])
   end
 
-  # POST /symptoms
-  # POST /symptoms.json
   def create
     @symptom = Symptom.new(params[:symptom])
 
-    respond_to do |format|
-      if @symptom.save
-        format.html { redirect_to @symptom, notice: 'Symptom was successfully created.' }
-        format.json { render json: @symptom, status: :created, location: @symptom }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @symptom.errors, status: :unprocessable_entity }
-      end
+    if @symptom.save
+      redirect_to @symptom, notice: 'Symptom was successfully created.'
+    else
+      render action: "new"
     end
   end
 
-  # PUT /symptoms/1
-  # PUT /symptoms/1.json
   def update
     @symptom = Symptom.find(params[:id])
 
-    respond_to do |format|
-      if @symptom.update_attributes(params[:symptom])
-        format.html { redirect_to @symptom, notice: 'Symptom was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @symptom.errors, status: :unprocessable_entity }
-      end
+    if @symptom.update_attributes(params[:symptom])
+      redirect_to @symptom, notice: 'Symptom was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /symptoms/1
-  # DELETE /symptoms/1.json
   def destroy
     @symptom = Symptom.find(params[:id])
     @symptom.destroy
 
-    respond_to do |format|
-      format.html { redirect_to symptoms_url }
-      format.json { head :no_content }
+    redirect_to symptoms_url
+  end
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "Sonia" && password == "123qwe"
     end
   end
 end
